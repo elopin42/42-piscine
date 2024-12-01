439decd87dfcf93975a6827e5fd41924d685acb1
Ethan Lopin <elopin@bess-f2r2s15.clusters.42paris.fr>
2024-12-01 21:01:08 +0100

    c00

---
+++ c00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elopin <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/18 22:41:48 by elopin            #+#    #+#             */
+/*   Updated: 2024/08/18 22:59:08 by elopin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	lettre;
+
+	lettre = 'z';
+	while (lettre >= 'a')
+	{
+		ft_putchar(lettre--);
+	}
+}
+//int main (void)
+//{
+//
+//    ft_print_reverse_alphabet();
+//    
+//}
---
+++ c00/ex06/ft_print_comb2.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elopin <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/16 21:42:58 by elopin            #+#    #+#             */
+/*   Updated: 2024/08/17 18:57:32 by elopin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_number(int n)
+{	
+	ft_putchar(n / 10 + '0');
+	ft_putchar(n % 10 + '0');
+}
+
+void	ft_print_comb2(void)
+{
+	int	a;
+	int	b;
+
+	a = 0;
+	b = 1;
+	while (a < 99)
+	{
+		while (b < 100)
+		{
+			ft_print_number(a);
+			ft_putchar(' ');
+			ft_print_number(b);
+			if (a != 98)
+			{
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+			b++;
+		}
+		a++;
+		b = a + 1;
+	}
+}
+
+//int	main(void)
+//{
+//	ft_print_comb2 ();
+//	return (0);
+//}
